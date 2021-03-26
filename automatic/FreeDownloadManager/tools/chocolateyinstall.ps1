a192b148756a6e2d28136c39827177ca0e7f3201
Shaun <github@gerstman.net>
2021-03-26 14:37:24 +1100

    Resolved merge conflicts

--- automatic/FreeDownloadManager/tools/chocolateyinstall.ps1
+++ automatic/FreeDownloadManager/tools/chocolateyinstall.ps1
@@ -9,11 +9,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   url64bit      = $url64
-<<<<<<< HEAD:automatic/FreeDownloadManager/tools/chocolateyinstall.ps1
   softwareName  = 'FreeDownloadManager*'
-=======
-  softwareName  = 'free-download-manager*'
->>>>>>> 7ecb6b8408657c08cdcf6cf2ffcb6f58757094a7:automatic/free-download-manager/tools/chocolateyinstall.ps1
   checksum      = 'E1B01688127F770B66A06438A87C54EC36B85393F779CCAEFE1127A3DECC6E97'
   checksumType  = 'sha256'
   checksum64    = '0B25D54D3DFE34B510D032AF9EA3AD14C345B9B8C2509C17BF102CB1D52A3CAD'
